2fec3f0040b75b5339bf1cafb310dbb7a6fce4a3
RockLee444 <lmatuz2211@gmail.com>
2020-03-20 14:31:00 -0600

    Uploading angular project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
 
+import { WebsocketService } from '../app/services/websocket.service'
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'Client';
+
+  constructor(public wsService: WebsocketService){
+  }
+
 }
